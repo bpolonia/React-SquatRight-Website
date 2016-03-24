83f3c54372ec36c7043408f95c848378224e4396
Brian Polonia <brian.polonia@gmail.com>
2016-03-24 14:36:00 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: __dirname + '/app/jsx/main.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css?modules!postcss'
+      }
+    ],
+
+  },
+  postcss: [
+    require('autoprefixer'),
+  ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: "./public",
+    colors: true,
+    inline: true,
+    hot: true
+  }
+}
